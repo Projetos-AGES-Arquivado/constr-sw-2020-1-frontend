1b7c79103fcf2bd418e42ee24f2e133ea7cffd56
ilegra.rodrigues <ilegra.rodrigues>
2020-06-08 20:25:19 -0300

    Adiciona estrutura basica do componente de Page Content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './screens/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageContentComponent } from './components/page-content/page-content.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PageContentComponent
   ],
   imports: [
     BrowserModule,
