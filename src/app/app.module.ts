55dab610d2f7f125811d42a3802c6b6f2802335c
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-06-08 11:23:44 -0300

    html and css of chips started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './screens/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChipListComponent } from './components/chip-list/chip-list.component';
+import { ChipComponent } from './components/chip/chip.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ChipListComponent,
+    ChipComponent
   ],
   imports: [
     BrowserModule,
