a63e1516b52977f9d1bde0fa8934904b54c34412
marceloAzevedo <marcelo@triider.com>
2020-06-07 23:38:12 -0300

    adding new component sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list'
+import { MatNativeDateModule, MatRippleModule} from '@angular/material/core';
 
 import { HomeComponent } from './screens/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [HomeComponent]
--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component ({
     selector: 'app-root',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
+
+
 
 export class HomeComponent{};
