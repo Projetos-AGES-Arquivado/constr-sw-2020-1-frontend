4f4d062220c45def421ac7f1851d5453675f33d3
Giuseppe Menti <mentifg@gmail.com>
2020-06-20 18:20:37 -0300

    Add building interface

--- src/app/screens/buildings-rooms/api.ts
+++ src/app/screens/buildings-rooms/api.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 
+export interface Building {
+  buildingID: string;
+  buildingName: string;
+  campus: string;
+  numberOfRooms: number;
+}
+
 export class BuildingRoomsApi {
   private baseURL: string = 'https://stark-gorge-03313.herokuapp.com';
 
   constructor(private httpClient: HttpClient) {}
 
-  async listBuildings() {
+  async listBuildings(): Promise<Building[]> {
     const response = await this.httpClient
       .get(`${this.baseURL}/buildings`)
       .toPromise();
-    return response;
+    return response as Building[];
   }
 }
